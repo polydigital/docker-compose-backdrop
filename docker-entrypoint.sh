5f96a794d4a69de54c16384728c3f93c3ece3c81
Tom Broughton <hello@tombroughton.me>
2019-07-11 23:16:13 +0100

    apache can now download components

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,11 @@
 cp /var/www/html/files-core/.htaccess /var/www/html/files/
 chown -R www-data:www-data /var/www/html/files/
 chmod -R 770 /var/www/html/files/
+chown -R www-data:www-data /var/www/html/modules/
+chmod -R 770 /var/www/html/modules/
+chown -R www-data:www-data /var/www/html/layouts/
+chmod -R 770 /var/www/html/layouts/
+chown -R www-data:www-data /var/www/html/themes/
+chmod -R 770 /var/www/html/themes/
+
 apache2-foreground
